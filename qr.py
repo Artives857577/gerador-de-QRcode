d48c71bc0ea86818c4efa9800997c52ed285464b
Artives857577 <109308619+Artives857577@users.noreply.github.com>
2023-04-23 22:27:25 -0300

    Update qr.py

--- qr.py
+++ qr.py
@@ -4,5 +4,5 @@ data = "https://www.aristidesmaltez.org.br/outras-opcoes/ "
 qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
 qr.add_data(data)
 qr.make(fit=True)
-img = qr.make_image(fil_color="black", back_color="white")
+img = qr.make_image(fil_color="black", back_color="white").
 img.save("qr_novo.png")
